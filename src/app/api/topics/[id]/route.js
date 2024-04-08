3c7c77d863f0ab947a665c4b26e11ea068acfb2d
Marinkie <tmarinkie12@gmail.com>
2024-04-09 01:31:05 +0200

    init: added update and get one topic

---
+++ src/app/api/topics/[id]/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import Topic from "../../../../../modules/topic";
+import connectMongoDB from "../../../../../libs/mongodb";
+
+export async function PUT(request,{params}){
+    
+    const {id} = params;
+    const {newTitle:title,newDescription:description} = await request.json();
+   
+        await connectMongoDB();
+        await Topic.findByIdAndUpdate(id, { title, description });
+        return NextResponse.json({message:"Topic Updated"},{status:200})
+
+}
+
+//get single topic by id
+export async function GET(request,{params}){
+    const {id}=params;
+    await connectMongoDB();
+    const topic = await Topic.findOne({_id:id})
+    return NextResponse.json({topic},{status:200})
+
+}
